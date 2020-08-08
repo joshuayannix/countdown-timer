d0185aaa1d5c7e2246905e4627a949784c5a56b0
Joshua Yannix <joshuayannix@gmail.com>
2020-08-08 11:56:54 -0500

    calculate ms

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('deadline');
 const items = document.querySelectorAll('.deadline-format h4')
 
-let futureDate = new Date(2020,9,25,13,53,0);
+let futureDate = new Date(2020,7,25,13,53,0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -39,3 +39,24 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `giveaway ends on ${weekday} ${month} ${date} ${year} ${hours}:${minutes}am`
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = Math.floor(t/oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let seconds = Math.floor((t % oneMinute) / 1000)
+  console.log(seconds)
+}
+
+getRemainingTime()
